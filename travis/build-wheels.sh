5e792e00beec014970e66c3cfb35f0c13ee10dd8
Dan <22e889d8@opayq.com>
2016-11-30 23:33:44 +0000

    Updated travis build wheels script

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -3,7 +3,8 @@ set -e -x
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    ${PYBIN}/pip install -r /io/dev-requirements.txt
+    ${PYBIN} pip install -U pip setuptools wheel
+    ${PYBIN}/pip install nose
     ${PYBIN}/pip wheel /io/ -w wheelhouse/
 done
 
